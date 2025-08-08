890caeb3511cf278c1b7587dede3cd47e2ad6f77
rohit tamra <rohit.tamra@rise-world.com>
2025-08-08 10:44:45 +0200

    adding k8s

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,3 +6,8 @@ output "kube_config" {
 output "cluster_name" {
   value = azurerm_kubernetes_cluster.aks.name
 }
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+  description = "The name of the resource group"
+}
